be83df5d9de604bc77106bf36e1d78437fc2a87c
Dan Jones <dan.jones@lunarfish.co.uk>
2021-11-05 21:34:35 +0000

    Update package to include component templates

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lunarfish/flask-auth",
     packages=setuptools.find_packages(),
+    package_data={
+        "staticauth": ["templates/*"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
